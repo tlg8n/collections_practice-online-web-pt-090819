1c72dff784d8d78815a7d044ac31a49a17881a6f
John Schmidley <tlg8n@outlook.com>
2019-09-29 12:19:57 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,25 @@
+def sort_array_asc(num)
+  num.sort do |a, b|
+  a <=> b
+end
+end
 
+def sort_array_desc(num)
+  num.sort {|x,y| y <=> x}
+end
+
+def sort_array_char_count(pets)
+pets.sort{|a, b| a.length <=> b.length}
+end
+
+def swap_elements(array)
+  array[0], array[1], array[2] = array[0], array[2], array[1]
+end
+
+def reverse_array(number)
+  number.reverse
+end
+
+def kesha_maker(array)
+  
+end
